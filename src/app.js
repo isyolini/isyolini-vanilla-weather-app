80dbb064080bac860c427776ede992752e230623
isyolini <50636149+isyolini@users.noreply.github.com>
2023-05-17 18:31:29 +0200

    Updated "description" position HTML

--- src/app.js
+++ src/app.js
@@ -31,6 +31,7 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
+  console.log(response.data);
 
   celsiusTemperature = response.data.temperature.current;
 
@@ -58,6 +59,18 @@ function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
+}
+
+////CURRENT LOCATION
+function searchLocation(position) {
+  let apiKey = "ce144f0cf51fa43f03431f0488a36728";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
 //UNITS CONVERTION
