0cf2e945e6806948782389ca3774b1f86133a102
isyolini <50636149+isyolini@users.noreply.github.com>
2023-05-23 16:20:31 +0200

    Implemantation forecast strip HTML/CSS

--- src/app.js
+++ src/app.js
@@ -61,7 +61,6 @@ function displayCurrentTemperature(response) {
 function searchCity(city) {
   let apiKey = "ate4fc772f93a185a4d70db0f2foe64c";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  //`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayCurrentTemperature);
 }
 
